52c55da9ff92319d793db1c648f5dfed9f154be6
caotu2k5 <71805958+caotu2k5@users.noreply.github.com>
2024-10-06 15:17:44 +0700

    Create inst.sh

---
+++ inst.sh
@@ -0,0 +1,7 @@
+clear
+echo "Đang cài đặt"
+apt update && apt upgrade -y
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v4aneko/raw/main/v4aneko.zip && unzip v4aneko.zip && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x mod && mv mod login 
+echo "Đã cài đặt thành công"
+login
