f4f8adfd4e13b501d2011e3b84b6f9d6040c6a6d
thangcualo <122159126+thangcualo@users.noreply.github.com>
2024-10-24 22:00:04 +0700

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -2,6 +2,6 @@ clear
 echo "Đang cài đặt"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v4aneko/raw/main/v4aneko.zip && pkg install unzip -y && unzip v4aneko.zip && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x mod && mv mod login 
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/thangcualo/v4aneko/raw/main/v4aneko.zip && pkg install unzip -y && unzip v4aneko.zip && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x mod && mv mod login 
 echo "Đã cài đặt thành công"
 login
